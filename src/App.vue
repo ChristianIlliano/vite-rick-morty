6b46382857d1c050b63a1a7a35c897b91f6e5bbe
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-06 15:18:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from "axios";
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  }
+data() {
+  return {
+    charArray: []
+  };
+},
+created() {
+  axios.get("https://rickandmortyapi.com/api/character", {
+    params: {
+
+    }
+  })
+  .then((resp) => {
+    this.charArray = resp.data.data;
+  })
+}
+}
+</script>
+
+<template>
+<AppHeader />
+<AppMain />
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
