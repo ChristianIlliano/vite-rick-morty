ce94982d80c5a4f6bd9a1b41872bb52de70b2fa4
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-07 15:40:40 +0200

    feat phase 4

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,51 @@ import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppChar from "./components/AppChar.vue";
+import AppSearch from "./components/AppSearch.vue";
 
 export default {
   components: {
     AppHeader,
     AppMain,
     AppChar,
+    AppSearch,
   },
   data() {
     return {
       charArray: [],
-  };
-},
-created() {
-  axios
-    .get("https://rickandmortyapi.com/api/character", {
-      params: {
-        id: 21,
-        offset:0,
-    },
-  })
-  .then((resp) => {
-    this.charArray = resp.data.results;
-    console.log(this.charArray, resp);
-  });
-},
+    };
+  },
+  created() {
+    axios
+      .get("https://rickandmortyapi.com/api/character", {
+        params: {
+          id: 21,
+          offset: 0,
+        },
+      })
+      .then((resp) => {
+        this.charArray = resp.data.results;
+        console.log(this.charArray);
+      });
+  },
+
+  methods: {
+    getStatus() {
+      console.log("stoCambinado");
+      // Costuriamo i parametri per la chiamata axios
+      //   const paramsObj = {
+      // }
+    }
+  },
+
 };
 </script>
 
 <template>
-<AppHeader />
-<AppMain :charArray="charArray"/>
+  <AppHeader />
+  <AppSearch @filter="getStatus()" />
+  <AppMain :charArray="charArray" />
 
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,34 @@
+<script>
+import {store} from "../store"
+export default {
+data() {
+    return {
+        store,
+        status: ["Alive", "unknown", "Dead"]
+    }
+},
+
+methods:{
+
+},
+};
+</script>
+
+<template>
+ <div class="container py-4">
+    <div class="d-flex justify-content-between">
+      <div>
+        <select @change="$emit('filter')" v-model="store.selectedStatus">
+          <option :value="statusAtm" v-for="statusAtm in status">
+            {{ statusAtm }}
+          </option>
+        </select>
+      </div>
+      <!-- <p>Mostrati {{ store.cardsList.length }} elementi</p> -->
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
